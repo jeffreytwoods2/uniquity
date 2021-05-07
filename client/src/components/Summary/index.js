32829cb9e010176949278204c9754fc43380d8d2
Jeffrey Woods <jeffreytwoods2@icloud.com>
2021-05-07 11:36:04 -0600

    Added header and summary components

---
+++ client/src/components/Summary/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Summary() {
+    return (
+        <p className="text-center fs-4" id="summary">
+            Every number sent through this site is saved to a database.
+            Try out a number and see if you're the first person on Earth to send it!
+        </p>
+    );
+}
+
+export default Summary;
