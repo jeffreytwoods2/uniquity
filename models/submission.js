58c72b6ff091c1b407c8be595621e2daaf386d99
Jeffrey Woods <jeffreytwoods2@icloud.com>
2021-05-10 19:48:59 -0600

    Added number submission model

---
+++ models/submission.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const NumberSchema = new Schema({
+    submission: {
+        type: Number,
+        unique: true,
+        match: [/^[0-9]+$/, "Please enter a valid integer"]
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Submission = mongoose.model("Number", NumberSchema);
+
+module.exports = Submission;
